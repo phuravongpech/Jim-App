b98e31a528864fa317664c0212b3998ce5128bb6
Hout <Houtkok123@gmail.com>
2025-02-15 16:03:16 +0700

    JIM 9 Intergrate API for Exercises

---
+++ backend/src/exercises/exercises.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import axios from 'axios';
+
+@Injectable()
+export class ExercisesService {
+    //Create to store Base URL endpoint
+    private readonly apiUrl = 'https://exercisedb.p.rapidapi.com/exercises';
+    //logger for track error
+    private readonly logger = new Logger(ExercisesService.name);
+
+    constructor(
+        private readonly configService: ConfigService
+    ){}
+    //get api header from env
+    private getApiOptions (){
+        return {
+            headers:{
+                'X-RapidAPI-KEY': this.configService.get<string>('EXERCISE_DB_API_KEY'),
+                'X-RapidAPI-Host': this.configService.get<string>('EXERCISE_DB_API_HOST'),
+            },
+        };
+    };
+    // fetch all exercise
+    async fetchAllExercisesFromApi(): Promise<any>{
+        try{
+            const response = await axios.get(this.apiUrl, this.getApiOptions());
+            return response.data;
+        }
+        catch(error){
+            this.logger.error('Error fetching exercises :', error);
+            throw error;
+        }
+    }
+
+}
