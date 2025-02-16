4ba9ba8b09f01f134e47e93aaa4f1da535b5425d
unknown <sinphourivath@gmail.com>
2025-02-16 13:24:35 +0700

    Refactor migration configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppConfig, DatabaseConfig } from './config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import AppDataSource from './config/data-source';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-      // load: [() => databaseConfig]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [AppConfig, DatabaseConfig],
     }),
-    TypeOrmModule.forRoot(
-      AppDataSource.options,
-    ),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -1,15 +1,15 @@
-import { DataSourceOptions } from 'typeorm';
+import { registerAs } from '@nestjs/config';
 
-const databaseConfig: DataSourceOptions = {
+export default registerAs('database', () => ({
   type: 'mysql',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '3306', 10),
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || 'jim',
-  entities: [__dirname + '/../entities/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
-  synchronize: false, 
-};
-
-export default databaseConfig;
+  port: parseInt(process.env.DB_PORT || "34532", 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
+  synchronize: process.env.NODE_ENV === 'development',
+  logging: process.env.NODE_ENV === 'development',
+  migrations: [`${__dirname}/../../db/migrations/*{.ts,.js}`],
+  migrationsTableName: 'migrations',
+}));
